bdc63afa68153157d20ae09c37432323ec99473d
Michael Romanenko <michael@romanenko.kg>
2020-03-12 20:54:39 +0600

    Pass firebase auth link along with exp:// deep link

--- pages/api/verify-email.tsx
+++ pages/api/verify-email.tsx
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase'
-import { parse as parseUrl } from 'url'
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -16,27 +15,10 @@ const REDIRECT_URL = 'exp://f3-d7k.romanenko.ololo.exp.direct:80'
 export default async (req, res) => {
   // Confirm the link is a sign-in with email link.
   if (firebase.auth().isSignInWithEmailLink(req.url)) {
-    // Additional state parameters can also be passed via URL.
-    // This can be used to continue the user's intended action before triggering
-    // the sign-in operation.
-    // Get the email if available. This should be available if the user completes
-    // the flow on the same device where they started it.
-    // The client SDK will parse the code from the link for you.
-    try {
-      const { query: { email } } = parseUrl(req.url, true)
+    res.writeHead(301, { Location: `${REDIRECT_URL}/?link=${encodeURIComponent(req.url)}` })
 
-      const result = await firebase
-        .auth()
-        .signInWithEmailLink(String(email), req.url)
-
-      console.log("User's e-mail verified", result)
-
-      res.writeHead(301, { Location: REDIRECT_URL })
-      res.end()
-    } catch (error) {
-      // Common errors could be invalid email and invalid or expired OTPs.
-      console.log(`Error verifying e-mail. Code: ${error.code}`)
-      console.log(error)
-    }
+    return res.end()
   }
+
+  res.status(406).end()
 }
